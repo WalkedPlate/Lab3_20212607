1fd84d7219793db1050ae20af91c02afc2b0d311
Arkel <144744565+WalkedPlate@users.noreply.github.com>
2024-04-18 20:38:05 -0500

    Initial commit

---
+++ src/main/java/com/example/lab3_20212607/Lab320212607Application.java
@@ -0,0 +1,13 @@
+package com.example.lab3_20212607;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab320212607Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab320212607Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lab3_20212607/Lab320212607ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab3_20212607;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab320212607ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
